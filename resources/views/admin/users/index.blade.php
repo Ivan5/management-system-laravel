39bfc41dee0c29588168d171d31fe458f4e3c847
Ivan5 <converse_ivan@hotmail.com>
2020-01-28 15:22:20 -0600

    Improving the admin user interface

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app') @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -8,9 +6,50 @@
                 <div class="card-header">Users</div>
 
                 <div class="card-body">
-                    @foreach ($users as $user)
-                      {{$user->name}} - {{$user->email}}
-                    @endforeach
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <a
+                                        href="{{ route('admin.users.edit', $user->id) }}"
+                                    >
+                                        <button
+                                            type="button"
+                                            class="btn btn-primary"
+                                        >
+                                            Edit
+                                        </button>
+                                    </a>
+
+                                    <a
+                                        href="{{
+                                            route('admin.users.destroy',$user->id)
+                                        }}"
+                                    >
+                                        <button
+                                            type="button"
+                                            class="btn btn-warning"
+                                        >
+                                            Delete
+                                        </button>
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
